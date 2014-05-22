128dc3ee3489dc9448ac99a979d5415163ab2ccf
t72 <nicomee.18@gmail.com>
2014-05-21 20:20:40 -0700

    Added modularity and new class file for reuse, removed redundant loops and conversions.

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using VigenereEncrypt;
+
 
 namespace VigenereEncrypt
 {
@@ -16,6 +18,8 @@ namespace VigenereEncrypt
         {
             InitializeComponent();
         }
+
+       
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
@@ -34,14 +38,29 @@ namespace VigenereEncrypt
 
         private void button1_Click(object sender, EventArgs e)
         {
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(); //Declares alphabet as a char array.
+
+            string message = textBox1.Text;
+            string userkey = textBox2.Text;
+            var instance = new VingereEncryption();
+            textBox3.Text = instance.Encrypt(sender, message, userkey);
+            
+            
+
+
+
+        }
+
+
+        public string encrypt(object sender, string message, string userkey)
+        {
+            Char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(); //Declares alphabet as a char array.
             string msg = textBox2.Text.ToString();
             string key = textBox1.Text.ToString();
             var chartest = new Regex("^[A-Za-z]");
 
             int keylength = key.Length;
             int msglength = msg.Length;
-         
+
             List<char> encryptedchar = new List<char>(); //Makes a new list for the characters to be stored in.
             List<int> encryptedlist = new List<int>(); //Makes a new list for the encrytped numbers to be stored in.
             List<int> msglist = new List<int>(); //LList will take conversion values for the message.
@@ -53,7 +72,7 @@ namespace VigenereEncrypt
                 {
                     int position = char.ToUpper(a) - 65;
                     msglist.Add(position);
-                   
+
                 }
 
                 foreach (char a in key)
@@ -73,7 +92,7 @@ namespace VigenereEncrypt
                     encryptedlist.Add(encrypted);
                 }
 
-               
+
             }
 
             foreach (int a in encryptedlist)
@@ -91,12 +110,13 @@ namespace VigenereEncrypt
 
             string encryptedmsg = builder.ToString();
             textBox3.Text = encryptedmsg;
+            return encryptedmsg;
 
 
 
-
-
-
+            
+            
+ 
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -111,67 +131,10 @@ namespace VigenereEncrypt
 
         private void button2_Click(object sender, EventArgs e)
         {
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(); //Declares alphabet as a char array.
-            string msg = textBox5.Text.ToString();
-            string key = textBox6.Text.ToString();
-            var chartest = new Regex("^[A-Za-z]");
-
-            int keylength = key.Length;
-            int msglength = msg.Length;
-
-            List<char> encryptedchar = new List<char>(); //Makes a new list for the characters to be stored in.
-            List<int> encryptedlist = new List<int>(); //Makes a new list for the encrytped numbers to be stored in.
-            List<int> msglist = new List<int>(); //LList will take conversion values for the message.
-            List<int> keylist = new List<int>(); //List will take conversion values for the key. 
-
-            if (chartest.IsMatch(key) && chartest.IsMatch(msg)) //Builds two arrays, one for the msg and one for the key.
-            {
-                foreach (char a in msg)
-                {
-                    int position = char.ToUpper(a) - 65;
-                    msglist.Add(position);
-                }
-
-                foreach (char a in key)
-                {
-                    int position = char.ToUpper(a) - 65;
-                    keylist.Add(position);
-                }
-
-
-                //First, we find the keyword number. We loop through the list using a for-loop.
-                for (int i = 0; i < msglist.Count; i++)
-                {
-
-                    int equivkey = i % keylength;
-                    int kj = keylist.ElementAt(equivkey);
-                    int nth = msglist.ElementAt(i);
-                    int decrypted = (nth - kj) % 26; //Actual "decryption" done here
-                    if (decrypted < 0)
-                    {
-                        decrypted = decrypted + 26; //Checks if answer is negative, since we are decrypting. 
-                    }
-                    encryptedlist.Add(decrypted);
-                }
-
-
-            }
-
-         foreach (int a in encryptedlist)
-            {
-              char position = alphabet[a];
-                encryptedchar.Add(position);
-           }
-
-            StringBuilder builder = new StringBuilder();
-            foreach (char a in encryptedchar)
-           {
-               builder.Append(a);
-           }
-
-
-           string encryptedmsg = builder.ToString();
-         textBox4.Text = encryptedmsg;
+            string message = textBox5.Text;
+            string userkey = textBox6.Text;
+            var instance = new VingereEncryption();
+            textBox4.Text = instance.Decrypt(sender, message, userkey);
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
---
+++ VingereEncryption.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VigenereEncrypt
+{
+    public class VingereEncryption
+    {
+
+        public string Encrypt(object sender, string userinput, string userkey)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Declares alphabet as a char array.
+            var chartest = new Regex("^[A-Za-z]");
+
+            int keylength = userkey.Length;
+            int msglength = userinput.Length;
+
+            List<char> encryptedchar = new List<char>(); //Makes a new list for the characters to be stored in.
+            List<int> encryptedlist = new List<int>(); //Makes a new list for the encrytped numbers to be stored in.
+            List<int> msglist = new List<int>(); //LList will take conversion values for the message.
+            List<int> keylist = new List<int>(); //List will take conversion values for the key. 
+
+            if (chartest.IsMatch(userkey) && chartest.IsMatch(userinput)) //Builds two arrays, one for the msg and one for the key.
+            {
+                foreach (char a in userinput)
+                {
+                    int position = char.ToUpper(a) - 'A';
+                    msglist.Add(position);
+
+                }
+
+                foreach (char a in userkey)
+                {
+                    int position = char.ToUpper(a) - 'A';
+                    keylist.Add(position);
+                }
+
+
+                //First, we find the keyword number. We loop through the list using a for-loop.
+                for (int i = 0; i < msglist.Count; i++)
+                {
+                    int equivkey = i % keylength;
+                    int kj = keylist.ElementAt(equivkey);
+                    int nth = msglist.ElementAt(i);
+                    int encrypted = (nth + kj) % 26;
+                    encryptedlist.Add(encrypted);
+                }
+
+
+            }
+
+
+            foreach (int a in encryptedlist)
+            {
+                char position = alphabet[a];
+                encryptedchar.Add(position);
+            }
+
+            StringBuilder builder = new StringBuilder();
+            foreach (char a in encryptedchar)
+            {
+                builder.Append(a);
+            }
+
+
+            string encryptedmsg = builder.ToString();
+            return encryptedmsg;
+        }
+        
+
+        public  string Decrypt(object sender, string userinput, string userkey)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(); //Declares alphabet as a char array
+            var chartest = new Regex("^[A-Za-z]");
+
+            int keylength = userkey.Length;
+            int msglength = userinput.Length;
+
+            List<char> encryptedchar = new List<char>(); //Makes a new list for the characters to be stored in.
+            List<int> encryptedlist = new List<int>(); //Makes a new list for the encrytped numbers to be stored in.
+            List<int> msglist = new List<int>(); //LList will take conversion values for the message.
+            List<int> keylist = new List<int>(); //List will take conversion values for the key. 
+
+            if (chartest.IsMatch(userkey) && chartest.IsMatch(userinput)) //Builds two arrays, one for the msg and one for the key.
+            {
+                foreach (char a in userinput)
+                {
+                    int position = char.ToUpper(a) - 65;
+                    msglist.Add(position);
+                }
+
+                foreach (char a in userkey)
+                {
+                    int position = char.ToUpper(a) - 65;
+                    keylist.Add(position);
+                }
+
+
+                //First, we find the keyword number. We loop through the list using a for-loop.
+                for (int i = 0; i < msglist.Count; i++)
+                {
+
+                    int equivkey = i % keylength;
+                    int kj = keylist.ElementAt(equivkey);
+                    int nth = msglist.ElementAt(i);
+                    int decrypted = (nth - kj) % 26; //Actual "decryption" done here
+                    if (decrypted < 0)
+                    {
+                        decrypted = decrypted + 26; //Checks if answer is negative, since we are decrypting. 
+                    }
+                    encryptedlist.Add(decrypted);
+                }
+
+
+            }
+
+            foreach (int a in encryptedlist)
+            {
+                char position = alphabet[a];
+                encryptedchar.Add(position);
+            }
+
+            StringBuilder builder = new StringBuilder();
+            foreach (char a in encryptedchar)
+            {
+                builder.Append(a);
+            }
+
+
+            string decryptedmsg = builder.ToString();
+            return decryptedmsg;
+        }
+    }
+}
